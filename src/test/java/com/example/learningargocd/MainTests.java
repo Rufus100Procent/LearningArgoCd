dd762a18bd250399a21592644e158fe5a547c017
stykle <Maxamadabdulahist@outlook.com>
2024-08-13 09:35:47 +0200

    Initial commit

---
+++ src/test/java/com/example/learningargocd/MainTests.java
@@ -0,0 +1,13 @@
+package com.example.learningargocd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
