f93dd3313ff47923302b4d0540e73c567c17d302
stykle <Maxamadabdulahist@outlook.com>
2024-08-13 13:00:20 +0200

    commit

--- src/main/java/com/example/learningargocd/Main.java
+++ src/main/java/com/example/learningargocd/Main.java
@@ -2,12 +2,20 @@ package com.example.learningargocd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class Main {
 
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
+    }
+
+    @GetMapping
+    public String hello() {
+        return "Hello World";
     }
 
 }
